61602215ab005bfcfc01a18674e7573e49ecdabc
lqz <273796790@qq.com>
2023-10-26 22:19:37 +0800

    dfs全排序

--- example/dfs_test.go
+++ example/dfs_test.go
@@ -5,24 +5,31 @@ import (
 	"testing"
 )
 
-/**
+/*
+*
 全排序：
 输入[1,2,3,4]
 输出所有不重复的排序可能
 */
 func TestDfs(t *testing.T) {
-	input := []int{1, 2, 3, 4}
-	out := struct {
-		result [][]int
-		table  map[string]bool
-	}{make([][]int, 0), make(map[string]bool, 0)}
+	input := []int{1, 2, 2, 3}
+	result := make([][]int, 0)
+	uniMap := make(map[string]bool, 0)
 	for i := 0; i < len(input); i++ {
-		dfs([]int{input[i]}, subSlice(input, i))
+		for _, v := range dfs([]int{input[i]}, subSlice(input, i)) {
+			s := toString(v)
+			if !uniMap[s] {
+				result = append(result, v)
+				uniMap[s] = true
+			}
+		}
 	}
-	fmt.Println(out.result)
+	fmt.Println(result)
+	// table := make(map[string]bool, 0)
 }
 
-/**
+/*
+*
 分析
 假设[1]的时候，拿到[2、3、4]进行不同组合组合
 假设[2]的时候，拿到[1、3、4]进行不同组合组合
@@ -32,18 +39,22 @@ func TestDfs(t *testing.T) {
 假设[1、3]的时候，拿到[2、4]进行不同组合组合
 假设[1、4]的时候，拿到[2、3]进行不同组合组合
 
-
 假设[1、2、3]的时候，拿到[4]仅有一个组合，则返回
 假设[1、2、4]的时候，拿到[3]仅有一个组合，则返回
 结社
 */
-func dfs(left []int, right []int) []int {
+func dfs(left []int, right []int) (out [][]int) {
 	if len(right) == 1 {
-		return append(left, right[0])
+		out = append(out, append(left, right[0]))
+		return
 	}
 	for i, v := range right {
-		dfs(append(left, v), subSlice(right, i))
+		r := dfs(append(left, v), subSlice(right, i))
+		for _, v := range r {
+			out = append(out, v)
+		}
 	}
+	return
 }
 
 func subSlice(input []int, i int) []int {
@@ -57,15 +68,12 @@ func subSlice(input []int, i int) []int {
 }
 
 /*
-去重复
+去重
 */
-func record(input []int) {
+func toString(input []int) string {
 	s := ""
 	for _, v := range input {
 		s += fmt.Sprintf("%d", v)
 	}
-	if !table[s] {
-		result = append(result, input)
-		table[s] = true
-	}
+	return s
 }
